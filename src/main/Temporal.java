7b22e5bff7ee94ed32bffd0e88dcf2d6d57c4b5a
wzseba <wzseba@gmail.com>
2023-10-24 23:25:03 -0300

    add: logica

--- src/main/Temporal.java
+++ src/main/Temporal.java
@@ -1,14 +1,19 @@
 package main;
 
-public class Temporal extends Empleado{
+public class Temporal extends Empleado {
 
 	private static double VALOR_HORA = 2000;
-	
+
 	public Temporal(double cantidadHorasTrabajadas, boolean casado, int cantidadHijos) {
 		super(cantidadHorasTrabajadas, casado, cantidadHijos);
 	}
-	
+
 	public double getSalario() {
-		return 0;
+
+		/*
+		 * Empleado de planta temporaria, no cobra antiguedad, solo cobra cantidad de
+		 * horas trabajadas por $2.000, mas salario familiar.
+		 */
+		return this.getCantidadHorasTrabajadas() * Temporal.VALOR_HORA + this.getSalarioFamiliar();
 	}
 }
