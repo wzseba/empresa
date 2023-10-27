31fe32359cb2faea7868f0cabbac11d779c4ed2f
wzseba <wzseba@gmail.com>
2023-10-26 23:29:14 -0300

    add: desarrollo de logica con metodos segun documentacion

--- src/main/Gerente.java
+++ src/main/Gerente.java
@@ -1,17 +1,27 @@
 package main;
 
-public class Gerente extends Permanente{
-	
-	
-	private static double VALOR_HORA = 4000;
-	private static double VALOR_ANTIGUEDAD = 1500;
+public class Gerente extends Permanente {
+
+	private static final double VALOR_HORA = 4000;
+	private static final double VALOR_ANTIGUEDAD = 1500;
 
 	public Gerente(double cantidadHorasTrabajadas, boolean casado, int cantidadHijos, int anioAntiguedad) {
 		super(cantidadHorasTrabajadas, casado, cantidadHijos, anioAntiguedad);
-		
+
+	}
+
+	public double getSalario() {
+
+		/*
+		 * Un gerente cobra de manera similar a un empleado de planta permanente pero
+		 * su hora trabajada vale$4.000, por antiguedad se le paga $1.500 por año,
+		 * mientras que el salario familiar es de el mismo que el de los empleados de
+		 * planta permanente y temporal.
+		 */
+		return super.getCantidadHorasTrabajadas() * Gerente.VALOR_HORA + getCalcularAntiguedad()  + super.getSalarioFamiliar();
 	}
 	
-	public double getSalario() {
-		return 0;
+	public double getCalcularAntiguedad() {
+		return super.getAnioAntiguedad() * Gerente.VALOR_ANTIGUEDAD;
 	}
 }
